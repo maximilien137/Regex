71edd283e34dacc4c18efafc705bd7211a3cd656
maximilien137 <105204121+maximilien137@users.noreply.github.com>
2022-08-19 14:02:12 +0200

    pipe

--- index.php
+++ index.php
@@ -10,17 +10,25 @@
     <h1>Je fais des Regex</h1>
     <?php
 
-        $masque = '/Michel/'; 
-        $tableau = ['Michel', 'Jean Michel', 'pieree michel', 'Robert', 'Arthur'];
-        $resultat = preg_grep($masque, $tableau);
+    $masque1 = "/ap|pe|ll/";
+    // $masque2 = "/[.]/"; 
+    // $masque3 = "/[\D]/"; 
+    // $masque4 = "/[\s]/"; 
+    $chaine = "Je m'appelle Jean-Louis, j'ai 42 ans";
 
-        var_dump($resultat);
+    preg_match_all($masque1, $chaine, $tableau1);
+    // preg_match_all($masque2, $chaine, $tableau2);
+    // preg_match_all($masque3, $chaine, $tableau3);
+    // preg_match_all($masque4, $chaine, $tableau4);
 
-       
-
-
+    var_dump($tableau1);
+    echo "<br />";
+    // var_dump($tableau2);
+    // echo "<br />";
+    // var_dump($tableau3);
+    // echo "<br />";
+    // var_dump($tableau4);
 
     ?>
 </body>
 </html>
-
